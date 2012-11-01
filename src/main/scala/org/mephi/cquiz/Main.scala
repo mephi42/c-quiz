35e8568f2d766f08c1bf8876c1739fd2d9ec3aed
Ilya <mephi42@gmail.com>
2012-11-01 19:03:12 +0400

    make sure generated code terminates and get answer

--- src/main/scala/org/mephi/cquiz/Main.scala
+++ src/main/scala/org/mephi/cquiz/Main.scala
@@ -1,7 +1,11 @@
 package org.mephi.cquiz
 
-class Main extends App {
+import java.nio.file.Files
+import java.io.{PrintStream, File}
+import scala.sys.process._
+import io.Source
 
+class Main extends App {
 }
 
 object Main {
@@ -11,5 +15,66 @@ object Main {
       quiz.question(writer)
       writer.write("return 0;").nextLine()
     }.nextLine().nextLine()
+  }
+
+  implicit def richProcessBuilder(pb: ProcessBuilder) = new {
+    def !!!() {
+      val code = pb.!
+      if (code != 0) sys.error("Nonzero exit value: " + code)
+    }
+  }
+
+  def answer(quiz: Quiz): Option[String] = {
+    val dir = Files.createTempDirectory("c-quiz").toFile
+
+    val src = new File(dir, "c-quiz.c")
+    val srcStream = new PrintStream(src)
+    try {
+      program(quiz, new DefaultWriter(srcStream))
+    } finally {
+      srcStream.close()
+    }
+
+    val bin = new File(dir, "c-quiz")
+    Process(Seq("gcc", "-O0", "-g3", "-o", bin.toString, src.toString)).!!!
+
+    val pyScript = new File(dir, "c-quiz.py")
+    val out = new File(dir, "c-quiz.out")
+    val pyStream = new PrintStream(pyScript)
+    try {
+      pyStream.print(
+        s"""
+          |gdb.execute("set args > $out")
+          |gdb.execute("set backtrace past-main")
+          |
+          |mainBp = gdb.Breakpoint("main")
+          |mainDoneBp = None
+          |
+          |count = 0
+          |def onStop(event):
+          |  global count, mainDoneBp
+          |  if isinstance(event, gdb.BreakpointEvent):
+          |    if mainBp in event.breakpoints:
+          |      mainDoneBp = gdb.FinishBreakpoint()
+          |    if mainDoneBp != None and mainDoneBp in event.breakpoints:
+          |      gdb.execute("c")
+          |  count = count + 1
+          |  if count > 100:
+          |    gdb.execute("quit 1")
+          |  gdb.execute("next")
+          |gdb.events.stop.connect(onStop)
+          |
+          |gdb.execute("run")
+        """.stripMargin
+      )
+    } finally {
+      pyStream.close()
+    }
+
+    val code = Process(Seq("gdb", "--batch", "-x", pyScript.toString, bin.toString)).!
+    if (code == 0)
+      Some(Source.fromFile(out).getLines().next())
+    else
+      None
   }
 }
--- src/test/scala/org/mephi/cquiz/loops/LoopsTest.scala
+++ src/test/scala/org/mephi/cquiz/loops/LoopsTest.scala
@@ -1,11 +1,11 @@
 package org.mephi.cquiz.loops
 
 import org.junit.Test
-import org.mephi.cquiz.{Main, DefaultWriter}
+import org.mephi.cquiz.Main
 
 class LoopsTest {
   @Test
   def test() {
-    Main.program(new Loops(), new DefaultWriter(Console.out))
+    println("answer: " + Main.answer(new Loops()))
   }
 }
