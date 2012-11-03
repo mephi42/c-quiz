5de7fca44ce3ff2525d596793cc577d86da04347
Ilya <mephi42@gmail.com>
2012-11-03 15:05:38 +0400

    webapp: reimplemented topic list using site map

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -3,9 +3,15 @@ package bootstrap.liftweb
 import net.liftweb._
 
 import http._
+import sitemap.{SiteMap, Loc, Menu}
 
 class Boot {
   def boot() {
     LiftRules.addToPackages("org.mephi.cquiz.liftweb")
+
+    val testEntries = org.mephi.cquiz.Topics.topics.map(t => Menu(Loc(t.id, List(t.id), t.title)))
+    val entries = Seq(Menu(Loc("index", List("index"), "Тесты по Си"), testEntries: _*))
+
+    LiftRules.setSiteMap(SiteMap(entries: _*))
   }
 }
--- src/main/scala/org/mephi/cquiz/liftweb/snippet/Topics.scala
+++
@@ -1,8 +0,0 @@
-package org.mephi.cquiz.liftweb.snippet
-
-import net.liftweb.util._
-import Helpers._
-
-class Topics {
-  def render = "li *" #> org.mephi.cquiz.Topics.topics.map(t => t.title)
-}
