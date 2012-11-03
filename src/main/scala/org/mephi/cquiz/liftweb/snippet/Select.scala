61a34ad4f5df5e9733cd1465c0cb6d41693afcff
Ilya <mephi42@gmail.com>
2012-11-04 00:32:15 +0400

    webapp: ajax submit button

--- src/main/scala/org/mephi/cquiz/liftweb/snippet/Select.scala
+++ src/main/scala/org/mephi/cquiz/liftweb/snippet/Select.scala
@@ -1,15 +1,26 @@
 package org.mephi.cquiz.liftweb.snippet
 
-import net.liftweb.http.SHtml
+import net.liftweb.http.{Templates, SHtml}
 import net.liftweb.util.Helpers._
 import xml.NodeSeq
 import org.mephi.cquiz.Topics
 import collection.mutable
+import net.liftweb.common.Full
+import net.liftweb.util.ClearNodes
+import net.liftweb.http.js.JsCmds.SetHtml
 
 class Select {
   def render = {
     var variantCount = "1"
     val topicCounts = new mutable.HashMap[String, Int]
+
+    def onSubmit = {
+      val template = Templates(List("quiz")) match {
+        case Full(x) => x
+        case _ => sys.error( """template "quiz" not found""")
+      }
+      SetHtml("#quiz", template)
+    }
 
     val variantCountTransform = "td id=variantCount *" #> SHtml.text(variantCount, variantCount = _, "size" -> "3")
     val topicCountsTransform = "tr id=topics" #> {
@@ -21,7 +32,9 @@ class Select {
         })
       }
     }
+    val submitTransform = "td id=submit" #> SHtml.ajaxSubmit("Получить тест", () => onSubmit)
+    val quizTransform = "div id=quiz *" #> ClearNodes
 
-    variantCountTransform `compose` topicCountsTransform
+    variantCountTransform `compose` topicCountsTransform `compose` submitTransform `compose` quizTransform
   }
 }
