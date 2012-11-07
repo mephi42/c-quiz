0d8c98ea984a260454f0803021e129736a618583
Ilya <mephi42@gmail.com>
2012-11-08 00:56:49 +0400

    added README

--- src/main/scala/org/mephi/cquiz/MakeSeeds.scala
+++ src/main/scala/org/mephi/cquiz/MakeSeeds.scala
@@ -4,17 +4,18 @@ import sjson.json._
 import DefaultProtocol._
 import JsonSerialization._
 import java.io.PrintStream
+import collection.parallel.mutable.ParArray
 
 object MakeSeeds extends App {
   args match {
     case Array(file, count) => {
       val seedsPerTopic: Int = count.toInt / Topics.topics.length
-      val seeds: Map[String, Array[(Long, String)]] = Topics.topics.map(topic => {
-        (topic.id, Array.fill(seedsPerTopic) {
+      val seeds: Map[String, Array[(Long, String)]] = Topics.topics.par.map(topic => {
+        (topic.id, ParArray.fill(seedsPerTopic) {
           val task = Main.makeTask(topic)
           (task.question.seed, task.answer)
-        })
-      }).toMap
+        }.seq.toArray)
+      }).seq.toMap
       val stream = new PrintStream(file)
       try {
         stream.println(tojson(seeds))
