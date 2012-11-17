fadda797b8fa80d65f08c9aa2f17e03f205cca76
Ilya <mephi42@gmail.com>
2012-11-17 23:31:24 +0400

    loops: reduced number of discarded questions

--- src/main/scala/org/mephi/cquiz/Loops.scala
+++ src/main/scala/org/mephi/cquiz/Loops.scala
@@ -1,6 +1,7 @@
 package org.mephi.cquiz
 
 import util.Random
+import collection.mutable.ListBuffer
 
 object Loops extends Topic {
   override val id = "loops"
@@ -35,30 +36,66 @@ object Loops extends Topic {
 
     override def seed = _seed
 
-    override val minSteps = 15
+    override val minSteps = 10
+
+    abstract class Relation(val text: String) {
+      def opposite: Relation
+
+      override def toString = text
+    }
+
+    object Less extends Relation("<") {
+      override def opposite = Greater
+    }
+
+    object Greater extends Relation(">") {
+      override def opposite = Less
+    }
+
+    object LessOrEqual extends Relation("<=") {
+      override def opposite = GreaterOrEqual
+    }
+
+    object GreaterOrEqual extends Relation(">=") {
+      override def opposite = LessOrEqual
+    }
+
+    object Equal extends Relation("==") {
+      override def opposite = Equal
+    }
+
+    object NotEqual extends Relation("!=") {
+      override def opposite = NotEqual
+    }
 
     private def loop(counter: String, writer: Writer)(body: => Any) {
       val start = rnd.nextInt(10)
       val end = rnd.nextInt(10)
       val delta = 1 + rnd.nextInt(3)
+      val deltaIsPositive = if (end >= start) true else false
 
       val loopCondition = {
-        val relation = rnd.nextInt(6) match {
-          case 0 => "<"
-          case 1 => "<="
-          case 2 => ">"
-          case 3 => ">="
-          case 4 => "=="
-          case 5 => "!="
+        val relations = {
+          val builder = new ListBuffer[Relation]
+          if (end >= start) {
+            builder ++= Seq(Less, LessOrEqual)
+          } else {
+            builder ++= Seq(Greater, GreaterOrEqual)
+          }
+          if ((end - start) % delta == 0) {
+            builder ++= Seq(Equal, NotEqual)
+          }
+          builder.toList
         }
+        val relation = relations(rnd.nextInt(relations.size))
         if (rnd.nextBoolean())
           counter + " " + relation + " " + end
         else
-          end + " " + relation + " " + counter
+          end + " " + relation.opposite + " " + counter
       }
       val initCounter = counter + " = " + start
       val nextCounter = {
-        if (rnd.nextBoolean()) {
+        if (deltaIsPositive) {
           counter + " += " + delta
         } else {
           counter + " -= " + delta
